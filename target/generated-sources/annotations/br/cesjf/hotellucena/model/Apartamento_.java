0a0f4af7f1da2e12649553ee7d3c7be56386ecba
Laura C <laurafariacastro@outlook.com>
2023-04-18 20:40:24 -0300

    necessary update post build

--- target/generated-sources/annotations/br/cesjf/hotellucena/model/Apartamento_.java
+++ target/generated-sources/annotations/br/cesjf/hotellucena/model/Apartamento_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-19T17:29:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-04-18T20:37:21")
 @StaticMetamodel(Apartamento.class)
 public class Apartamento_ { 
 
